8506e43214b504991d3bbf3e1ccd14d14897d49b
dennis-lai96 <dennis.lai96@gmail.com>
2023-04-08 20:31:40 -0700

    Correct sprite animations + movement

--- SpriteMovement.py
+++ SpriteMovement.py
@@ -1,51 +1,78 @@
 from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
+import time
+class Sprite:
+    def __init__(self, canvas, x, y, images):
+        self.canvas = canvas
+        self.images = [ImageTk.PhotoImage(Image.open(img)) for img in images]
+        self.frame = 0
+        self.id = canvas.create_image(x,y,image=self.images[self.frame])
+
+    def update(self):
+        self.frame = (self.frame+1)%len(self.images)
+        self.canvas.itemconfig(self.id, image=self.images[self.frame])
+
+    def move_left(self):
+        self.canvas.move(self.id, -10, 0)
+
+    def move_right(self):
+        self.canvas.move(self.id, 10, 0)
+
+    def move_up(self):
+        self.canvas.move(self.id, 0, -10)
+
+    def move_down(self):
+        self.canvas.move(self.id, 0, 10)
 
 
-root = Tk()
-root.title('Sprite Movement')
-root.geometry("800x600")
+root = tk.Tk()
+canvas = tk.Canvas(root, width=4000, height=4000)
+canvas.pack()
 
-w=600
-h=400
-x=w/2
-y=h/2
+sprite_images = ['Sprites/idle/0.png','Sprites/idle/1.png',
+                 'Sprites/idle/2.png','Sprites/idle/3.png',
+                 'Sprites/idle/4.png','Sprites/idle/5.png',
+                 'Sprites/idle/6.png','Sprites/idle/7.png',
+                 'Sprites/idle/8.png','Sprites/idle/9.png',
+                 'Sprites/idle/10.png','Sprites/idle/11.png']
 
-my_canvas = Canvas(root,width=w,height=h,bg="white")
-my_canvas.pack(pady=20)
+sprite = Sprite(canvas, 100, 100, sprite_images)
 
-img = PhotoImage(file="Sprites/idle/0.png")
-my_image= my_canvas.create_image(200,150, anchor=NW,image = img)
+def handle_key(event):
+    if event.keysym == 'Up':
+        sprite.move_up()
+        print("up")
+    elif event.keysym == 'Down':
+        sprite.move_down()
+    elif event.keysym == 'Left':
+        sprite.move_left()
+    elif event.keysym == 'Right':
+        sprite.move_right()
 
-def left(event):
-    x = -10
-    y= 0
-    my_canvas.move(my_image,x,y)
-def right(event):
-    x = 10
-    y= 0
-    my_canvas.move(my_image,x,y)
-def up(event):
-    x = 0
-    y= -10
-    my_canvas.move(my_image,x,y)
-def down(event):
-    x = 0
-    y= 10
-    my_canvas.move(my_image,x,y)
-
-
-root.bind("<a>", left)
-root.bind("<d>", right)
-root.bind("<w>", up)
-root.bind("<s>",down)
-
+canvas.bind_all('<KeyPress>', handle_key)
+while True:
+    sprite.update()
+    canvas.update()
+    time.sleep(0.0005)
 
 
 
 
 
 root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
